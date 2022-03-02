e5a9fc523a47911691dd19ff7fc4a47b35974586
Eniyadharsan <eniyadharsan280@gmail.com>
2022-03-02 12:32:12 +0530

    Updated Doxygen

---
+++ 3_Implementation/Documentation/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['documentation_5fmain_0',['documentation_main',['../md__c___users__eniyan__documents__project___1__m1__projectgoal_app_3__implementation_inc_documentation_main.html',1,'']]]
+];
---
+++ 3_Implementation/inc/documentation_main.md
@@ -0,0 +1,2 @@
+	@mainpage Calculator Application by "Eniyadharsan D"
+	@subpage calculator_operations.h
